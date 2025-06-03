22f321168bc3a4115c1f5cbf3bd829633ca26acb
Melvineitor <melvinpaulino0019ceges@gmail.com>
2025-06-03 15:09:54 -0400

    Update environment.prod.ts

--- inmo/src/environments/environment.prod.ts
+++ inmo/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://localhost:5095/api',
+  apiURL: 'https://backend-production-7cbc.up.railway.app/api',
   version: '1.0.0',
   debugMode: false
 };
