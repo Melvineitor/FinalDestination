74cd68cb4923aff8312698ba2eadd8be2dc116b5
Melvineitor <melvinpaulino0019ceges@gmail.com>
2025-06-05 02:27:15 -0400

    tailwind

--- inmo/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ inmo/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ba6df19510e2fd91c3efa7d4edb570963a793e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7321c0740ae4f281e96a29701abe1d50c77f5d73")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
