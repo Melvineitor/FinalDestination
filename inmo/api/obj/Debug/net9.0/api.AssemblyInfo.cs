0ff3e7f5df104bf22c56dc340d0e5fb57ad7066e
Melvineitor <melvinpaulino0019ceges@gmail.com>
2025-06-12 01:14:24 -0400

    ventas

--- inmo/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ inmo/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb7bc34fc46e10d634612e2ea85ad311edbead4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292e8b1af094bce2dad881689ae418029aedfff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
