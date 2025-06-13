dc068aa4d5ce16482b2a6c3e3905059031190d22
Melvineitor <melvinpaulino0019ceges@gmail.com>
2025-06-13 19:47:32 -0400

    transaccion

--- inmo/src/app/pago/registrar-pago/registrar-pago.component.ts
+++ inmo/src/app/pago/registrar-pago/registrar-pago.component.ts
@@ -43,49 +43,21 @@ propiedades: any;
 
   constructor(private fb: FormBuilder, private inmoService: InmoService, private router: Router) {
     this.registroForm = this.fb.group({
-      fecha_pago: ['', Validators.required],
-      metodo_pago: ['', Validators.required],
-      motivo_pago: ['', Validators.required],
-      desc_pago: ['', Validators.required],
-      tipo_contrato: ['', Validators.required],
-      descripcion_penalidad: [null],
-      // Campos de tarjeta
-      num_tarjeta: [''],
-      tipo_tarjeta: [''],
-      titular_tarjeta: [''],
-      fecha_venc: [''],
-      cvv: [''],
-      compania_tarjeta: [''],
-      monto_pagado: ['', [Validators.required, Validators.min(0)]],
-      estado_pago: ['', Validators.required],
+      id_inmueble: ['', Validators.required],
+      tipo_transaccion: ['', Validators.required],
+      fecha_transaccion: ['', Validators.required],
+      monto_transaccion: ['', Validators.required],
+      id_inquilino: ['', Validators.required],
       nombre_agente: ['', Validators.required],
-      persona_pago: ['', Validators.required]
     });
   }
 
   ngOnInit() {
-    // Inicialización si es necesaria
+    this.inmoService.getPropiedades().subscribe((data) => {
+      this.propiedades = data;
+    });
   }
 
-  onMetodoPagoChange(event: any) {
-    const metodoPago = event.target.value;
-    this.mostrarCamposTarjeta = metodoPago === 'Tarjeta';
-    
-    if (!this.mostrarCamposTarjeta) {
-      // Si no es tarjeta, resetear los campos de tarjeta
-      this.registroForm.patchValue({
-        num_tarjeta: '',
-        tipo_tarjeta: '',
-        titular_tarjeta: '',
-        fecha_venc: '',
-        cvv: '',
-        compania_tarjeta: ''
-      });
-      this.tarjetaSeleccionada = null;
-      this.tarjetasEncontradas = [];
-      this.mostrarFormularioNuevaTarjeta = false;
-    }
-  }
 
   buscarTarjeta() {
     const searchTerm = this.searchTarjetaControl.value;
@@ -108,46 +80,7 @@ propiedades: any;
     });
   }
 
-  seleccionarTarjeta(event: any) {
-    const tarjetaId = parseInt(event.target.value);
-    if (!tarjetaId) return;
-
-    this.inmoService.obtenerTarjeta(tarjetaId).subscribe({
-      next: (tarjeta) => {
-        this.tarjetaSeleccionada = tarjeta;
-        this.mostrarFormularioNuevaTarjeta = false;
-        
-        // Actualizar el formulario con los datos de la tarjeta seleccionada
-        this.registroForm.patchValue({
-          num_tarjeta: tarjeta.num_tarjeta,
-          tipo_tarjeta: tarjeta.tipo_tarjeta,
-          titular_tarjeta: tarjeta.titular_tarjeta,
-          fecha_venc: tarjeta.fecha_venc,
-          cvv: tarjeta.cvv,
-          compania_tarjeta: tarjeta.compania_tarjeta
-        });
-      },
-      error: (error) => {
-        console.error('Error al obtener los detalles de la tarjeta:', error);
-        alert('Error al obtener los detalles de la tarjeta');
-      }
-    });
-  }
-
-  toggleNuevaTarjeta() {
-    this.mostrarFormularioNuevaTarjeta = !this.mostrarFormularioNuevaTarjeta;
-    if (!this.mostrarFormularioNuevaTarjeta) {
-      // Limpiar campos de tarjeta si se cancela
-      this.registroForm.patchValue({
-        num_tarjeta: '',
-        tipo_tarjeta: '',
-        titular_tarjeta: '',
-        fecha_venc: '',
-        cvv: '',
-        compania_tarjeta: ''
-      });
-    }
-  }
+ 
 
   async onSubmit() {
     if (this.registroForm.invalid) {
@@ -156,44 +89,16 @@ propiedades: any;
     }
 
     try {
-      let tarjetaId: number | null = null;
 
-      if (this.registroForm.get('metodo_pago')?.value === 'Tarjeta') {
-        if (this.tarjetaSeleccionada) {
-          // Usar tarjeta existente
-          tarjetaId = this.tarjetaSeleccionada.id_tarjeta;
-        } else if (this.mostrarFormularioNuevaTarjeta) {
-          // Crear nueva tarjeta
-          const tarjetaData = {
-            num_tarjeta: this.registroForm.get('num_tarjeta')?.value,
-            tipo_tarjeta: this.registroForm.get('tipo_tarjeta')?.value,
-            titular_tarjeta: this.registroForm.get('titular_tarjeta')?.value,
-            fecha_venc: this.registroForm.get('fecha_venc')?.value,
-            cvv: this.registroForm.get('cvv')?.value,
-            compania_tarjeta: this.registroForm.get('compania_tarjeta')?.value
-          };
-
-          const tarjetaResponse = await this.inmoService.crearTarjeta(tarjetaData).toPromise();
-          tarjetaId = tarjetaResponse.id_tarjeta;
-        }
-      }
-
-      // Preparar datos del pago
       const pagoData = {
-        fecha_pago: this.registroForm.get('fecha_pago')?.value,
-        metodo_pago: this.registroForm.get('metodo_pago')?.value,
-        motivo_pago: this.registroForm.get('motivo_pago')?.value,
-        desc_pago: this.registroForm.get('desc_pago')?.value,
-        tipo_contrato: this.registroForm.get('tipo_contrato')?.value,
-        descripcion_penalidad: this.registroForm.get('descripcion_penalidad')?.value,
-        compania_tarjeta: tarjetaId, // Puede ser null si es efectivo
-        monto_pagado: this.registroForm.get('monto_pagado')?.value,
-        estado_pago: this.registroForm.get('estado_pago')?.value,
+        fecha_transaccion: this.registroForm.get('fecha_transaccion')?.value,
+        tipo_transaccion: this.registroForm.get('tipo_transaccion')?.value,
+        monto_transaccion: this.registroForm.get('monto_transaccion')?.value,
+        id_inquilino: this.registroForm.get('id_inquilino')?.value,
+        id_inmueble: this.registroForm.get('id_inmueble')?.value,
         nombre_agente: this.registroForm.get('nombre_agente')?.value,
-        persona_pago: this.registroForm.get('persona_pago')?.value
       };
 
-      // Crear el pago
       await this.inmoService.crearPago(pagoData).toPromise();
       alert('Pago registrado exitosamente');
       this.router.navigate(['/pago']);
